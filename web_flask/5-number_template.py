be51f8955cef3bfc093018bb8ee1b2fcd674b67a
Ngonidzashe Brandon Towindo <ngoni@live.com>
2023-02-11 01:01:50 +0200

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 The script starts Flask web application
-    listening on 0.0.0.0, port 5000
+    listen on 0.0.0.0, port 5000
     routes: /:                    display "Hello HBNB!"
             /hbnb:                display "HBNB"
             /c/<text>:            display "C" + text (replace "_" with " ")
@@ -17,40 +17,38 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def helloo_hbnb():
-    """Display the text"""
+    """Display the  text"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb')
 def hbnb():
-    """Display the text"""
+    """display text"""
     return "HBNB"
 
 
 @app.route('/c/<text>')
 def custom_text(text):
-    """Display custom text"""
+    """Display custom text: """
     return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python')
 @app.route('/python/<text>')
 def python_text(text="is cool"):
-    """Dsplay custom text given"""
+    """Display custom text: """
     return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>')
 def txt_if_int(n):
-    """Dsplay text only if intis given"""
+    """Display text only if int is given"""
     return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>')
-def html_given_int(n)
-    """Display html page only if int given
-       place given int into html template
-    """
+def html_if_int(n):
+    """Display html pages only if int is given"""
     return render_template('5-number.html', n=n)
 
 
